acd34361b71959e8c3c22314015ac5eea60c8dd9
Nitul Datt <nitul1991@gmail.com>
2019-05-30 21:35:21 +0530

    Added test cases for unique_and_copy_dups

--- tests/unique_and_copy_dups.cpp
+++ tests/unique_and_copy_dups.cpp
@@ -20,3 +20,69 @@ TEST_CASE("empty source", "[unique_and_copy_dups]")
                &&
                dups.size() == 0 ) );
 }
+
+TEST_CASE("source without dups", "[unique_and_copy_dups]")
+{
+    auto source = std::vector<int>{1, 2, 3};
+    auto dups   = std::vector<int>{};
+
+    auto result = mstl::unique_and_copy_dups(std::begin(source)
+                                            ,std::end(source)
+                                            ,std::back_inserter(dups)
+                                            ,std::equal_to<int>{});
+
+    REQUIRE( ( result.first == std::end(source)
+               &&
+               dups.size() == 0 ) );
+}
+
+TEST_CASE("source with dups", "[unique_and_copy_dups]")
+{
+    auto source = std::vector<int>{1, 1, 1, 2, 2, 3};
+    auto dups   = std::vector<int>{};
+
+    auto result = mstl::unique_and_copy_dups(std::begin(source)
+                                            ,std::end(source)
+                                            ,std::back_inserter(dups)
+                                            ,std::equal_to<int>{});
+
+    REQUIRE( ( std::vector<int>{begin(source), result.first} == std::vector<int>{1, 2, 3}
+               &&
+               dups == std::vector<int>{1, 1, 2} ) );
+}
+
+TEST_CASE("source of pairs with dups", "[unique_and_copy_dups]")
+{
+    auto source = std::vector<std::pair<std::string, int>> {
+         {"one", 1}
+        ,{"one", 2}
+        ,{"two", 2}
+        ,{"three", 3}
+        ,{"three", 4}
+    };
+    auto dups   = std::vector<std::pair<std::string, int>>{};
+
+    auto const is_equal = [](std::pair<std::string, int> const & left
+                            ,std::pair<std::string, int> const & right)
+    {
+        return left.first == right.first;
+    };
+
+    auto result = mstl::unique_and_copy_dups(std::begin(source)
+                                            ,std::end(source)
+                                            ,std::back_inserter(dups)
+                                            ,is_equal);
+
+    REQUIRE( ( std::vector<std::pair<std::string, int>>{begin(source), result.first}
+               ==
+               std::vector<std::pair<std::string, int>> {
+                    {"one", 1}
+                   ,{"two", 2}
+                   ,{"three", 3}
+               }
+               &&
+               dups == std::vector<std::pair<std::string, int>> {
+                    {"one", 2}
+                   ,{"three", 4}
+               } ) );
+}
